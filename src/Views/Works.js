9feb419f932130e6351dffbdf548f29965fb7ce0
Fackiu <facuupaez95@gmail.com>
2022-01-03 20:46:40 -0300

    view work is responsive

--- src/Views/Works.js
+++ src/Views/Works.js
@@ -3,9 +3,9 @@ import catWorking from '../assets/img/catWorking.jpg'
 
 const Works = () => {
     return (
-        <div className="center-align">
+        <div className="container center-align">
            <h3>Sorry! This site is in production </h3>
-           <img src={catWorking} alt='siteInProduction' style={{ maxWidth: '30rem', marginTop: '1rem', marginBottom: '1rem', borderRadius: '10%' }} />
+           <img src={catWorking} alt='siteInProduction' style={{ maxWidth: '20rem', marginTop: '1rem', marginBottom: '1rem', borderRadius: '10%' }} />
         </div>
     )
 }
